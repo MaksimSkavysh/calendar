64eb8e8b0aae9f6edd9b34be5750ffeab858b565
Nickolay <rudoy.kolya@gmail.com>
2019-01-20 20:11:48 +0300

    Creating hostess-admin app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+    <Button color="primary">primary</Button>,
+    document.getElementById('root'))
+
