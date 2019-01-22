dfc0f11d19d3bde88faa80b0dec9f2976dc6c4ad
n.rudoy <n.rudoy@logic-way.com>
2019-01-22 14:25:11 +0300

    hostess-admin. add classnames, style guide improvement

--- src/components/WeekPicker/WeekPicker.js
+++ src/components/WeekPicker/WeekPicker.js
@@ -9,12 +9,14 @@ import './WeekPicker.scss'
 class WeekPicker extends PureComponent {
     render() {
         return (
-            <div className={'week-picker'}>
-                <DayRow name={dayName}/>
-                <div>
-                    {hours.map(item => (
-                        <DayRow key={item} name={item}/>
-                    ))}
+            <div className={'week-picker-position'}>
+                <div className={'week-picker'}>
+                    <DayRow name={dayName}/>
+                    <div>
+                        {hours.map(item => (
+                            <DayRow key={item} name={item}/>
+                        ))}
+                    </div>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom'
 import WeekPicker from './components/WeekPicker/WeekPicker'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.scss'
+
 
 ReactDOM.render(
-    <div
-        className={'week-picker-position'}>
-        <WeekPicker/>
-    </div>,
+    <WeekPicker/>,
     document.getElementById('root'))
