c782c73314cc1f55003910da024f0abdf42d1269
Nickolay <rudoy.kolya@gmail.com>
2019-01-25 23:57:43 +0300

    swap class component to functional component

--- src/components/WeekPicker/WeekPicker.js
+++ src/components/WeekPicker/WeekPicker.js
@@ -1,26 +1,24 @@
-import React, {PureComponent} from 'react'
+import React from 'react'
 
 import DayRow from '../DayRow/DayRow'
-import {dayName, hours} from "../../constants"
+import {dayName, hours} from '../../constants'
 
 import style from './WeekPicker.module.scss'
 
 
-class WeekPicker extends PureComponent {
-    render() {
-        return (
-            <div className={style.weekPickerPosition}>
-                <div className={style.weekPicker}>
-                    <DayRow name={dayName}/>
-                    <div>
-                        {hours.map(item => (
-                            <DayRow key={item} name={item}/>
-                        ))}
-                    </div>
+const WeekPicker = () => {
+    return (
+        <div className={style.weekPickerPosition}>
+            <div className={style.weekPicker}>
+                <DayRow name={dayName}/>
+                <div>
+                    {hours.map(item => (
+                        <DayRow key={item} name={item}/>
+                    ))}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default WeekPicker
