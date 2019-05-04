3f27061268f7a45e5b1f4f663e5fc25406ba36f1
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-05-04 10:25:45 +0300

    Add tests

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -8,50 +8,52 @@ import styles from './schedule.module.scss'
 const DIVISIONS_NUMBER = 24
 const ITEM_HEIGHT = 20
 
-const state = {
-  1557003600000: [makeInterval(12, 16), makeInterval(20, 23)],
-}
-
-const startData = moment(new Date()).startOf('day').valueOf()
-const daysAmount = 7
-
-const getDaysList = (startDate, amount) => {
-  const toDate = (d) => moment(startDate).add(d, 'days')
-  return R.compose(R.map(toDate), R.range(0))(amount)
-}
-
-const getValue = d => d.valueOf()
-const dateToInterval = (intervalsMap) =>
-  R.compose(R.defaultTo([]), R.flip(R.prop)(intervalsMap), getValue)
-
-const intervalStyles = (i) => ({
-  top: intervalStart(i) * ITEM_HEIGHT,
-  height: intervalDuration(i) * ITEM_HEIGHT,
+const intervalStyles = (interval) => ({
+  top: intervalStart(interval) * ITEM_HEIGHT,
+  height: intervalDuration(interval) * ITEM_HEIGHT,
 })
+const Interval = interval => (<div
+  className={styles.intervalItem}
+  key={intervalStart(interval)}
+  style={intervalStyles(interval)}
+/>)
+const IntervalItems = R.map(Interval)
 
-const IntervalItems = R.map(R.compose(
-  s => (<div className={styles.intervalItem} style={s} />),
-  intervalStyles,
-))
-
-const Column = ({ column }) => (<div
+const Column = ({ date, schedule }) => (<div
+  key={date}
   className={styles.column}
   style={{ height: ITEM_HEIGHT * DIVISIONS_NUMBER }}
 >
-  {IntervalItems(column)}
+  {IntervalItems(schedule || [])}
 </div>)
+const ColumnsList = R.map(R.compose(Column))
+
+const getDaysList = (startDate, amount) => {
+  const toDate = (d) => moment(startDate).add(d, 'days').valueOf()
+  return R.compose(R.map(toDate), R.range(0))(amount)
+}
+
+export const scheduleFromDate = (intervalsMap) => R.applySpec({
+  date: R.identity,
+  schedule: date => intervalsMap[date] || [],
+})
 
 const Schedule = () => {
+  const config = {
+    1557003600000: [makeInterval(12, 16), makeInterval(20, 23)],
+  }
+  const startData = moment(new Date()).startOf('day').valueOf()
+  const daysAmount = 7
   const daysList = getDaysList(startData, daysAmount)
-  const names = R.map(d => d.format('ddd'))(daysList)
-  const columns = R.map(dateToInterval(state))(daysList)
+  const names = R.map(d => moment(d).format('ddd'))(daysList)
+  const columns = R.map(scheduleFromDate(config), daysList)
   return (
     <div>
       <div className={styles.header}>
         {names.map(name => (<div className={styles.headerItem} key={name}>{name}</div>))}
       </div>
       <div className={styles.body}>
-        {columns.map(column => (<Column column={column} />))}
+        {ColumnsList(columns)}
       </div>
     </div>
   )
