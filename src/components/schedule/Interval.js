ba14c5bf91b1a42e56a534caf2c6b39ba4287149
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-05-18 22:29:40 +0300

    Refactor interval

--- src/components/schedule/Interval.js
+++ src/components/schedule/Interval.js
@@ -6,15 +6,14 @@ import { intervalDuration, intervalStart } from 'data/intervals'
 
 import styles from './schedule.module.scss'
 
-const intervalStyles = (interval) => ({
-  top: intervalStart(interval) * ITEM_HEIGHT,
-  height: intervalDuration(interval) * ITEM_HEIGHT,
-})
+const heightMultiply = R.multiply(ITEM_HEIGHT)
+const getTop = R.compose(heightMultiply, intervalStart)
+const getHeight = R.compose(heightMultiply, intervalDuration)
+const intervalStyles = R.applySpec({ top: getTop, height: getHeight })
 
 const Interval = interval => (<div
   className={styles.intervalItem}
   key={intervalStart(interval)}
   style={intervalStyles(interval)}
 />)
-
 export const IntervalItems = R.map(Interval)
