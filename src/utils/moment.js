ad95553dc2f878ba5c5c68cbd4c15c89213e338f
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-05-19 17:43:08 +0300

    Fix to moment

--- src/utils/moment.js
+++ src/utils/moment.js
@@ -1,5 +1,5 @@
 import * as R from 'ramda'
 import moment from 'moment'
 
-export const toMoment = d => moment(d)
+export const toMoment = d => moment.isMoment(d) ? d : moment(d)
 export const format = R.curry((str, m) => m.format(str))
