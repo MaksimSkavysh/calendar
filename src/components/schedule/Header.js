e31a6f9e413c1e9b6be940a698b94eef7361ee37
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-05-22 16:34:27 +0300

    Improove some styles

--- src/components/schedule/Header.js
+++ src/components/schedule/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import * as R from 'ramda'
+import Typography from '@material-ui/core/Typography'
+import Chip from '@material-ui/core/Chip'
 import { format } from 'utils/moment'
 
 import styles from './schedule.module.scss'
 
 const headerItemParams = R.applySpec({ name: format('ddd'), number: format('DD') })
 const HeaderItemRender = ({ name, number }) => (
-  <div className={styles.headerItem} key={name + number} >
-    <span>{name}</span>
-    <span>{number}</span>
+  <div className={styles.headerItem} key={name + number}>
+    <Typography>{name}</Typography>
+    <Chip label={number} />
   </div>
 )
 const HeaderItems = R.map(R.compose(HeaderItemRender, headerItemParams))
